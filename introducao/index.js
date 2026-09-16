7d7f87f8f97f86b605e0c7f9711f65ff319c06e6
Gustavo <gustavo.guimaraes2148@gmail.com>
2026-09-15 21:47:20 -0300

    update

--- introducao/index.js
+++ introducao/index.js
@@ -1,9 +1,42 @@
+const dados = [];
+
+const foto = document.getElementById("foto");
+const mensagem = document.getElementById("mensagem");
+const cor = document.getElementById("cor");
+
 function clicou() {
     var nome = document.getElementById("nome").value;
-    alert(nome);
+    var senha = document.getElementById("senha").value;
+    var data = document.getElementById("data").value;
+
+    var infos = {
+        nome,
+        senha,
+        data,
+    }
+
+    dados.push(infos);
+
+    if (nome && senha && data) {
+        alert("Olá " + nome + "!\n" +
+            "Senha: " + senha + "\n" +
+            "Data: " + data + "\n" +
+            "Seus dados foram salvos com sucesso.");
+
+            document.getElementById("nome").value = "";
+            document.getElementById("senha").value = "";
+            document.getElementById("data").value = "";
+
+            cor.classList.remove('escondido');
+            mensagem.classList.remove('escondido');
+            foto.classList.remove('escondido');
+    } else {
+        alert("Por favor, preencha todos os campos antes de enviar!");
+    }
+
 }
 
 var seletorCor = document.getElementById("cor");
-seletorCor.addEventListener("input", function() {
-    document.body.style.backgroundColor = seletorCor.value
+seletorCor.addEventListener("input", function () {
+    document.body.style.backgroundColor = seletorCor.value;
 })
